e51af7052740791a381f491452ee421f1e94022a
Fahad Ameen <fahad.amin@codingo.onmicrosoft.com>
2023-11-11 09:56:33 +0100

    changes

--- src/Libraries/Nop.Services/Transactions/TransactionService.cs
+++ src/Libraries/Nop.Services/Transactions/TransactionService.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Core.Domain.Transaction;
 
 namespace Nop.Services.Transactions
 {
     public partial class TransactionService : ITransactionService
     {
+        public Task DeleteTransactionAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IPagedList<Transaction>> GetAllTransactionsAsync(DateTime startOnUtc = default, DateTime endOnUtc = default, int customerId = 0, decimal balance = 0, int transactionTypeId = 0, int statusId = 0, int pageSize = int.MaxValue, int pageIndex = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Transaction> GetTransactionByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertTransactionAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTransactionAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
